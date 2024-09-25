855e406057498de833a3d355c659a948f9465ba0
Juan Gimenez Corbalan <juagimcor@alu.edu.gva.es>
2024-09-25 16:26:23 +0200

    Working U3P1

--- serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
+++ serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
@@ -13,14 +13,25 @@ namespace GesPresta
         {
 
         }
-
         protected void cmdEnviar_Click(object sender, EventArgs e)
         {
             
         }
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
+            string cadena = "";
 
+            if (true)
+            {
+                
+            }
+            cadena = cadena + "Código: " + Request.Form["txtCodPre"] + "<br/>";
+            cadena = cadena + "Descripción: " + Request.Form["txtDesPre"] + "<br/>";
+            cadena = cadena + "Importe: " + Request.Form["txtImpPre"] + "<br/>";
+            cadena = cadena + "Porcentaje: " + Request.Form["txtPorPre"] + "<br/>";
+            cadena = cadena + "Tipo de Prestación: " + Request.Form["ddlTipPre"] + "<br/>";
+            lblValores.Text = cadena;
+            lblValores.Visible = true;
         }
     }
 }
