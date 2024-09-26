cec2e927f0ba72a7d1db9477c18a9618ab34495e
gimenezjuan <gimenezcorbalan@gmail.com>
2024-09-26 10:58:33 +0200

    Working on exercise 3

--- serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
+++ serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -17,6 +18,19 @@ namespace GesPresta
         protected void cmdEnviar_Click(object sender, EventArgs e)
         {
             
+        }
+        private bool CheckTimes(DateTime date)
+        {
+            string validateDate = @"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$";
+
+            Regex regex = new Regex(validateDate);
+
+            if (!regex.IsMatch(validateDate))
+            {
+                return false;
+            }
+
+            return true;
         }
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
@@ -44,6 +58,26 @@ namespace GesPresta
             DateTime dtIngression = Calendar2.SelectedDate;
             DateTime dtToday = System.DateTime.Now;
             DateTime dtBday = Calendar1.SelectedDate;
+
+            int year = dtToday.Year - dtIngression.Year;
+            int month = dtToday.Month - dtIngression.Month;
+            int days = dtToday.Day - dtIngression.Day;
+
+            if (days < 0)
+            {
+                month--;
+                days += DateTime.DaysInMonth(dtIngression.Year, dtIngression.Month);  
+            }
+
+            if (month < 0)
+            {
+                year--;
+                month += 12; 
+            }
+
+            txtAños.Text = year.ToString();
+            TxtMeses.Text = month.ToString();
+            txtDias.Text = days.ToString();
 
             string cadena = "";
 
