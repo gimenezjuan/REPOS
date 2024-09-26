5f44f832c608fdccf50082b913015cacf7cceb4f
Juan Gimenez Corbalan <juagimcor@alu.edu.gva.es>
2024-09-26 16:58:37 +0200

    Working on exercise 4

--- serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
+++ serv/U3P1/Acme/Acme/GesPresta/EmpleadosCalendar.aspx.cs
@@ -13,7 +13,9 @@ namespace GesPresta
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            lblError1.Visible = false;
+            lblError2.Visible = false;
+            lblError3.Visible = false;
         }
         protected void cmdEnviar_Click(object sender, EventArgs e)
         {
@@ -29,80 +31,81 @@ namespace GesPresta
             {
                 return false;
             }
-
             return true;
+        }
+        private void CalculateSenior(DateTime dIngr, DateTime dToday)
+        {
+            TimeSpan diferencia = dToday - Calendar2.SelectedDate;
+            DateTime fechamin = new DateTime(1, 1, 1);
+
+            int year = dToday.Year - dIngr.Year;
+            int month = dToday.Month - dIngr.Month;
+            int days = dToday.Day - dIngr.Day;
+
+            txtAños.Text = ((fechamin + diferencia).Year - 1).ToString();
+            TxtMeses.Text = ((fechamin + diferencia).Month - 1).ToString();
+            txtDias.Text = ((fechamin + diferencia).Day).ToString();
+
+            if (dIngr > dToday && days < 0)
+            {
+                lblError2.Visible = true;
+                txtFinEmp.Text = "";
+                txtAños.Text = "";
+                TxtMeses.Text = "";
+                txtDias.Text = "";
+            }
+            else
+            {
+                txtAños.Text = ((fechamin + diferencia).Year - 1).ToString();
+                TxtMeses.Text = ((fechamin + diferencia).Month - 1).ToString();
+                txtDias.Text = ((fechamin + diferencia).Day).ToString();
+            }
+
+        }
+        private void CheckDates(DateTime dBday, DateTime dIngr, DateTime dToday)
+        {
+            string cadena = "";
+
+            if (dIngr != DateTime.MinValue)
+            {
+                if (dIngr < dBday)
+                {
+                    cadena = cadena + "La fecha de ingreso no puede ser menor que la fecha del cumpleaños" + "\n";
+                    lblError1.Text = cadena;
+                    lblError1.Visible = true;
+                }
+            }
+            if (dIngr > dToday)
+            {
+                cadena = cadena + "La fecha de ingreso no puede se mayor que la fecha actual" + "\n";
+                lblError2.Text = cadena;
+                lblError2.Visible = true;
+            }
+            if (dBday > dToday)
+            {
+                cadena = cadena + "La fecha de cumpleaños no puede se mayor que la fecha actual" + "\n";
+                lblError3.Text = cadena;
+                lblError3.Visible = true;
+            }
         }
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
             txtFnaEmp.Text = Calendar1.SelectedDate.ToShortDateString();
             DateTime dtToday = System.DateTime.Now;
+            DateTime dtIngression = Calendar2.SelectedDate;
             DateTime dtBday = Calendar1.SelectedDate;
-
-            string cadena = "";
-
-            if (dtBday.Date > dtToday.Date)
-            {
-                cadena = cadena + "La fecha de cumpleaños no puede se mayor que la fecha actual" + "\n";
-                lblError1.Text = cadena;
-                lblError1.Visible = true;
-            }
-            else
-            {
-                lblError1.Visible = false;
-            }
-
+            CheckDates(dtBday, dtIngression, dtToday);
+       
         }
         protected void Calendar2_SelectionChanged(object sender, EventArgs e)
         {
             txtFinEmp.Text = Calendar2.SelectedDate.ToShortDateString();
-            DateTime dtIngression = Calendar2.SelectedDate;
             DateTime dtToday = System.DateTime.Now;
             DateTime dtBday = Calendar1.SelectedDate;
+            DateTime dtIngression = Calendar2.SelectedDate;
 
-            int year = dtToday.Year - dtIngression.Year;
-            int month = dtToday.Month - dtIngression.Month;
-            int days = dtToday.Day - dtIngression.Day;
-
-            if (days < 0)
-            {
-                month--;
-                days += DateTime.DaysInMonth(dtIngression.Year, dtIngression.Month);  
-            }
-
-            if (month < 0)
-            {
-                year--;
-                month += 12; 
-            }
-
-            txtAños.Text = year.ToString();
-            TxtMeses.Text = month.ToString();
-            txtDias.Text = days.ToString();
-
-            string cadena = "";
-
-            if (dtIngression.Date > dtToday.Date)
-            {
-                cadena = cadena + "La fecha de ingreso no puede ser mayor que la fecha actual" + "\n";
-                lblError2.Text = cadena;
-                lblError2.Visible = true;
-            }
-            else
-            {
-                lblError2.Visible = false;
-            }
-
-            if (dtIngression.Date < dtBday.Date)
-            {
-                cadena = cadena + "La fecha de ingreso no puede ser menor que la fecha del cumpleaños" + "\n";
-                lblError3.Text = cadena;
-                lblError3.Visible = true;
-            }
-            else
-            {
-                lblError3.Visible = false;
-            }
-
+            CheckDates(dtBday, dtIngression, dtToday);
+            CalculateSenior(dtIngression, dtToday);
         }
     }
 }
